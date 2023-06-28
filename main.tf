d4466dc85667180e8f993db9191a8d20f01b12c4
chandanadevopslearn <128459202+chandanadevopslearn@users.noreply.github.com>
2023-06-28 12:48:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instancecee" {
   ami           = "ami-0261755bbcb8c4a84"
-  instance_type = "t2.micro"
-  key_name = "pemkey"
+  instance_type = "t2.large"
+  key_name = "ansible"
 }
